62b332781eea28af159568d04a39d2af609df507
Thomas Menu <55690394+DrTriac@users.noreply.github.com>
2020-03-13 15:39:52 +0100

    alles in de git gesmeten

---
+++ togglescript.js
@@ -0,0 +1,55 @@
+function togglefunction_squirrel() {
+    let x = document.getElementById("subscriptionInfo_squirrel");
+    if (x.style.display === "none")
+    {
+        x.style.display = "flex";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+ }
+
+ function togglefunction_penguin() {
+    let x = document.getElementById("subscriptionInfo_penguin");
+    if (x.style.display === "none")
+    {
+        x.style.display = "flex";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+ 
+ }
+
+ function togglefunction_owl() {
+    let x = document.getElementById("subscriptionInfo_owl");
+    if (x.style.display === "none")
+    {
+        x.style.display = "flex";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+ 
+ }
+
+ function togglefunction_fox() {
+    let x = document.getElementById("subscriptionInfo_fox");
+    if (x.style.display === "none")
+    {
+        x.style.display = "flex";
+    }
+    else 
+    {
+        x.style.display = "none";
+    }
+ 
+ }
+
+
+ 
+
+ 
